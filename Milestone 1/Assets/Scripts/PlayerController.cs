d730de4f3cf40f2cc1fc547703ec5742eb95bf19
Jason Libbey <jelgt2011@gmail.com>
2015-09-27 09:00:48 -0400

    M1: almost done
    add background terrain and then host

---
+++ Milestone 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    protected Animator _animator;
+    public Animator Animator
+    {
+        get { return _animator; }
+        protected set { _animator = value; }
+    }
+
+    public float Forward = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!_animator)
+        {
+            _animator = GetComponentInChildren<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.anyKey)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                Forward = 0.5f;
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    Forward = 1f;
+                }
+                
+                _animator.SetFloat("forward", Forward);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _animator.SetTrigger("jump");
+            }
+
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                _animator.SetTrigger("left_attack");
+            }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                _animator.SetTrigger("center_combo");
+            }
+
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                _animator.SetTrigger("right_attack");
+            }
+
+        } else
+        {
+            Forward = 0f;
+            _animator.SetFloat("forward", Forward);
+        }
+    }
+}
